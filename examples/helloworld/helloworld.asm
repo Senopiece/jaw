db03ba80394fb616fb77cfdc22240bbb1bccc4e5
Nabuki <shichiromail@gmail.com>
2023-12-15 00:46:53 +0300

    fix ident

--- examples/helloworld/helloworld.asm
+++ examples/helloworld/helloworld.asm
@@ -23,8 +23,8 @@
 //    `data` must be binary (if started with 0xb) or hexadecimal (if started with 0x)
 //    `*created_labels` must be a sequence of hexadecimal numbers delimited by spaces
 
-reg0: 0x0 = 0x0                                                               // reg0 must be only 0 or 1, init with 0 (this macro will actually generate no code)
-reg1: 0x0 = 0x2                                                               // set reg1 to 2 (generates reg1[-2] = 1)
+reg0: 0x0 = 0x0 // reg0 must be only 0 or 1, init with 0 (this macro will actually generate no code)
+reg1: 0x0 = 0x2 // set reg1 to 2 (generates reg1[-2] = 1)
 
 // the current augmented avriable/label syntax:
 
